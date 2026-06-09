1677859253d0a804e2f142aa5b18af981a89556f
Makena Kong <kena.kong@gmail.com>
2026-06-08 22:38:32 -0700

    new nav

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,10 +6,10 @@ export interface INavLink {
 }
 
 const defaultNavLinks: INavLink[] = [
-  { title: "dev", to: "/dev" },
-  { title: "art", to: "/art" },
-  { title: "design", to: "/design" },
-  { title: "contact me", to: "/contact" }
+  { title: "Software", to: "/dev" },
+  { title: "Artwork", to: "/art" },
+  { title: "Design", to: "/design" },
+  { title: "Contact Me", to: "/contact" }
 ]
 
 const props = defineProps<{ links?: INavLink[] }>()
@@ -36,10 +36,11 @@ nav ul {
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  gap: var(--space-1);
 
-  font-family: var(--font-sans);
-  font-weight: bold;
+  font-family: var(--font-space);
+  font-size: var(--text-sm);
+  letter-spacing: -5%;
+  font-weight: light;
 
   list-style-type: none;
 }
@@ -52,7 +53,7 @@ nav ul a {
 
 
 nav ul a:hover {
-  color: var(--magenta);
+  color: var(--purple);
 }
 
 nav img {
@@ -60,18 +61,23 @@ nav img {
 }
 
 .active-link {
-  color: var(--purple)
+  color: var(--purple);
+  text-decoration: underline;
+}
+
+.link-star {
+  padding: 4px
 }
 </style>
 
 <template>
   <nav>
     <NuxtLink to="/">
-      <img src="@/assets/images/logo.svg" />
+      <img src="@/assets/images/logo.svg" alt="mak logo" />
     </NuxtLink>
     <ul>
       <li v-for="link in navLinks" :key="link.to">
-        <NuxtLink :to="link.to" exact-active-class="active-link">{{ link.title }}</NuxtLink>
+        <NuxtLink :to="link.to" exact-active-class="active-link">{{ link.title }}</NuxtLink><span class="link-star">✶</span>
       </li>
     </ul>
   </nav>
